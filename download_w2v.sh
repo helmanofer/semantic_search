86c80808701672a0f8d5721564d66a36c11704d7
Cloud User <ec2-user@ip-172-31-23-181.us-east-2.compute.internal>
2020-09-19 12:51:44 +0000

    using word2vec
    Signed-off-by: Cloud User <ec2-user@ip-172-31-23-181.us-east-2.compute.internal>

---
+++ download_w2v.sh
@@ -0,0 +1,5 @@
+ gdown  https://drive.google.com/uc?id=10nmmD_rwBdEHHI_Dx-1w1ZlKpJrCtdNA
+ gdown  https://drive.google.com/uc?id=IwAR1-C-z-qOarKTVX3hiSHXqOI9dRG3seMQG1J4tXscoaXTY6auDf_nJrwhg
+ gdown  https://drive.google.com/uc?id=1vjCvQnrPN0ZgDM1bPEbGp-rnqCtHhdOL
+ gdown  https://drive.google.com/uc?id=1MODwRw1HzOWUa5CoMZxULm0OB9in1-aT
+ gdown  https://drive.google.com/uc?id=1qFzsPltNwBOYBH3af0ZE7Scl51gu13at
--- utils/index.py
+++ utils/index.py
@@ -2,6 +2,10 @@ import os
 import xml.etree.ElementTree as ET
 from lsh.random_projection import LshGaussianRandomProjection
 import numpy
+from gensim.models.doc2vec import Doc2Vec
+
+# from utils import common
+
 
 import json
 
@@ -9,16 +13,15 @@ import json
 # tar = tarfile.open("/mnt/c/SourceCode/sematic_search/tapuz_xml2.tar.gz")
 # tar.extractall()
 # tar.close()
-from utils.common import get_project_root
-import fasttext
+# import fasttext
+# ft = fasttext.load_model("cc.he.300.bin")
 
 
 def read_tapuz_data():
     print("loading fasttext")
-    ft = fasttext.load_model("cc.he.300.bin")
     print("done loading fasttext")
     directory = "/mnt/c/SourceCode/sematic_search/xml"
-    directory = os.path.join(get_project_root(), "xml")
+    directory = os.path.join(common.get_project_root(), "xml")
     _id = 0
     with open("tapuz.jsonl", "w", encoding="utf-8") as fw:
         for fname in os.listdir(directory):
@@ -50,15 +53,40 @@ def read_tapuz_data():
                 _id += 1
 
 
-def read_tapuz_data_with_vec():
-    lsh_g = LshGaussianRandomProjection(
-        vector_dimension=300, bucket_size=4, num_of_buckets=40, seed=4
-    )
+def create_w2v_file():
+    model = Doc2Vec.load("/dev/models/dbow_w3_EP15_yes_remove_stp")
+    lsh_g = LshGaussianRandomProjection(vector_dimension=300, bucket_size=4, num_of_buckets=40, seed=4)
     lsh_g.fit()
-    with open("tapuz_with_vectors.jsonl") as rf:
+
+    with open("tapuz.jsonl") as rf, open("tapuz_with_w2v_lsh.jsonl", "w", encoding="utf-8") as wf:
+        _id = 0
         for line in rf:
             j = json.loads(line)
-            for d in j["paragraphs"]:
-                vec = d.pop("par_vector")
-                d["lsh"] = " ".join(lsh_g.indexable_transform(numpy.array(vec)))
+            pars = []
+            data = {
+                "_id": _id,
+                "text": " ".join(["\n".join(l) for l in j]),
+                "paragraphs": pars,
+            }
+            for p in j:
+                vec = model.infer_vector(p)
+                lsh = " ".join(lsh_g.indexable_transform(vec))
+                # lsh = "sdfsdfdf"
+                par = {
+                        "par_text": " ".join(p),
+                        "par_tokens": p,
+                        "lsh": lsh,
+                    }
+                pars.append(par)
+            wf.write(json.dumps(data, ensure_ascii=False) + "\n")
+            _id += 1
+
+
+def read_tapuz_data_with_vec():
+    with open("tapuz_with_w2v_lsh.jsonl") as rf:
+        for line in rf:
+            j = json.loads(line)
             yield j
+
+
+# create_w2v_file()
